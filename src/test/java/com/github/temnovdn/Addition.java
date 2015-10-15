456b5c1176db5a691fa22f71619b54609ff604bc
d.temnov <d.temnov@plamee.com>
2015-10-15 18:15:55 +0600

    Created project structure

---
+++ src/test/java/com/github/temnovdn/Addition.java
@@ -0,0 +1,9 @@
+package com.github.temnovdn;
+
+import ru.yandex.qatools.allure.annotations.TestCaseId;
+
+/**
+ * Created by dmitrytemnov on 15/10/15.
+ */
+public class Addition {
+}
---
+++ src/test/java/com/github/temnovdn/Multyplying.java
@@ -0,0 +1,7 @@
+package com.github.temnovdn;
+
+/**
+ * Created by dmitrytemnov on 15/10/15.
+ */
+public class Multyplying {
+}
