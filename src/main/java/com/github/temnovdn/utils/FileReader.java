05a17362de0e73685ab9b7d9ac164273a846c338
Dmitry Temnov <temnovdn@gmail.com>
2015-10-25 16:46:44 +0600

    Implemented tests for all operations

--- src/main/java/com/github/temnovdn/utils/FileReader.java
+++ src/main/java/com/github/temnovdn/utils/FileReader.java
@@ -1,5 +1,8 @@
 package com.github.temnovdn.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -9,6 +12,8 @@ import java.util.ArrayList;
  * Created by dmitrytemnov on 16/10/15.
  */
 public class FileReader {
+
+    static final Logger LOGGER = LoggerFactory.getLogger(FileReader.class);
 
     public static ArrayList<String[]> readFile(final String filepath) {
         ArrayList<String[]> operations = new ArrayList<String[]>();
@@ -24,6 +29,7 @@ public class FileReader {
                 operations.add(row);
             }
         } catch (FileNotFoundException e) {
+            LOGGER.info(String.format("File %s not found", filepath));
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
--- src/test/java/com/github/temnovdn/OperationsTest.java
+++ src/test/java/com/github/temnovdn/OperationsTest.java
@@ -1,11 +1,14 @@
 package com.github.temnovdn;
 
 import com.github.temnovdn.utils.FileReader;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import com.github.temnovdn.AssertWrapper;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,34 +16,67 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by dmitrytemnov on 16/10/15.
  */
+@RunWith(value = Parameterized.class)
 public class OperationsTest {
+    private String operand1;
+    private String operand2;
+    private String operation;
+    private String result;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OperationsTest.class);
 
+    public OperationsTest(String operand1, String operand2, String operation, String result) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+        this.operation = operation;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static List<String[]> testData() {
+
+        final ArrayList<String[]> data = FileReader.readFile("src/main/resourses/testfile");
+
+        return data;
+    }
+
     @Test
     public void test() {
-        final ArrayList<String[]> operations = FileReader.readFile("src/main/resourses/testfile");
 
-        for (String[] row: operations ) {
-            Integer operand1 = Integer.parseInt(row[0]);
-            Integer operand2 = Integer.parseInt(row[1]);
-            char operation = row[2].charAt(0);
-                double result = Double.valueOf(row[3]);
+        LOGGER.info(String.format("Testing: %s %s %s = %s", operand1, operation, operand2, result));
 
-            switch (operation) {
-                case '+':
-                    AssertWrapper.plusAndAssert(operand1, operand2, result);
-                    break;
-                case '-':
-                    AssertWrapper.minusAndAssert(operand1, operand2, result);
-                    break;
-                case '*':
-                    AssertWrapper.multiplyAndAssert(operand1, operand2, result);
-                    break;
-                case '/':
-                    AssertWrapper.divideAndAssert(operand1, operand2, result);
+        if (operation.equals("+")) {
+            int expectedResult = Integer.parseInt(result);
+            int actualResult = Integer.parseInt(operand1) + Integer.parseInt(operand2);
+            LOGGER.info(String.format("Actual result: %s", result));
+            Assert.assertEquals(expectedResult, actualResult);
+
+        } else if (operation.equals("-")) {
+            int expectedResult = Integer.parseInt(result);
+            int actualResult = Integer.parseInt(operand1) - Integer.parseInt(operand2);
+            LOGGER.info(String.format("Actual result: %s", result));
+            Assert.assertEquals(expectedResult, actualResult);
+
+        } else if (operation.equals("*")) {
+            int expectedResult = Integer.parseInt(result);
+            int actualResult = Integer.parseInt(operand1) * Integer.parseInt(operand2);
+            LOGGER.info(String.format("Actual result: %s", result));
+            Assert.assertEquals(expectedResult, actualResult);
+
+        } else if (operation.equals("/")) {
+            float expectedResult = Float.parseFloat(result);
+            float actualResult;
+            try {
+                actualResult = Integer.parseInt(operand1) / Integer.parseInt(operand2);
+                LOGGER.info(String.format("Actual result: %s", result));
+                Assert.assertEquals(expectedResult, actualResult, 1e-10);
+            } catch (ArithmeticException ex) {
+                LOGGER.info("Actual result: / by zero exception caught");
+                Assert.assertEquals(ex.getMessage(), "/ by zero");
             }
-        }
 
+        } else {
+            LOGGER.info("Wrong operation type");
+        }
     }
 }
